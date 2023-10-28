bc2e7fc88d12a98e6d654f52b35b8c71f263183c
ShibnathFrontEndDeveloper <132988977+ShibnathFrontEndDeveloper@users.noreply.github.com>
2023-10-28 12:37:41 +0530

    Update about.blade.php

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     @include('user.includes.favicon')
 
-    <title>Future Lift</title>
+    <title>Future Lift1</title>
     @include('user.includes.css-link')
     @section('csss')
     <link
