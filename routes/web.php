3174751487fe407d59b1b96fc74ef159b45c9765
rajdeep19031997 <97425582+rajdeep19031997@users.noreply.github.com>
2024-01-02 19:58:12 +0530

    issue solved all

--- routes/web.php
+++ routes/web.php
@@ -89,6 +89,7 @@ Route::get('/instant-advice',[HomeController::class , 'InstantAdviceIndex']);
 Route::get('/get-library-data/{key}/{categoryId}',[HomeController::class , 'getLibraryData']);
 Route::get('/career-library-post-like/{id}',[HomeController::class , 'careerLibraryPostLike']);
 Route::post('/career-library-post-dislike',[HomeController::class , 'careerLibraryPostDisLike']);
+Route::get('/career-library-search',[HomeController::class , 'indexCareerLibrarySearch']);
 
 
 
